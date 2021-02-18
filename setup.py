2e7804bda882cdecc91b2335e4d27a2267f6fde1
Daan van den Bergh <vandenberghinc.contact@gmail.com>
2021-02-18 08:53:04 +0100

    Updates
    updates.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@
 from setuptools import setup, find_packages
 setup(
 	name='inc-package-manager',
-	version='2.1.0',
+	version='2.1.1',
 	description='Some description.',
 	url="http://github.com/vandenberghinc/vandenberghinc-package-manager",
 	author='Daan van den Bergh',
