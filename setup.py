cbbd8a3d705a9e839f6d39feb48bf4403c75052c
Daan van den Bergh <vandenberghinc.contact@gmail.com>
2021-03-04 12:28:26 +0100

    Updates
    updates.

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 
 # Note!
-# ' are required, do not use any ".
+# ' are required, do not use any '.
 
 # setup.
 from setuptools import setup, find_packages
 setup(
 	name='inc-package-manager',
-	version='2.8.0',
+	version='2.8.1',
 	description='Some description.',
-	url="http://github.com/vandenberghinc/vandenberghinc-package-manager",
+	url='http://github.com/vandenberghinc/vandenberghinc-package-manager',
 	author='Daan van den Bergh',
 	author_email='vandenberghinc.contact@gmail.com',
 	license='MIT',
@@ -17,5 +17,9 @@ setup(
 	include_package_data=True,
 	zip_safe=False,
 	install_requires=[
-		#"requests",
-	],)
+            'cl1>=1.11.8',
+            'netw0rk>=1.7.5',
+            'r3sponse>=2.8.6',
+            'syst3m>=2.13.2',
+            'fil3s>=2.12.8',
+        ],)
