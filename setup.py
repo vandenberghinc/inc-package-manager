02496dc96aa66601e80589000fd5080d00987f09
Daan van den Bergh <vandenberghinc.contact@gmail.com>
2021-03-08 14:56:44 +0100

    Updates
    updates.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@
 from setuptools import setup, find_packages
 setup(
 	name='inc-package-manager',
-	version='2.9.5',
+	version='2.9.6',
 	description='Some description.',
 	url='http://github.com/vandenberghinc/vandenberghinc-package-manager',
 	author='Daan van den Bergh',
@@ -17,9 +17,9 @@ setup(
 	include_package_data=True,
 	zip_safe=False,
 	install_requires=[
-            'cl1>=1.13.1',
-            'netw0rk>=1.8.9',
-            'r3sponse>=2.10.2',
-            'syst3m>=2.16.1',
-            'fil3s>=2.15.4',
+            'cl1>=1.13.2',
+            'netw0rk>=1.9.0',
+            'r3sponse>=2.10.3',
+            'syst3m>=2.16.4',
+            'fil3s>=2.15.7',
         ],)
